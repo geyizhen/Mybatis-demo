66704c937466cbe3d053920ffc3c19f2e24e3271
geyizhen <381339594@qq.com>
2020-09-03 13:54:03 +0800

    Mybatis-demo

---
+++ src/main/java/com/sample/domin/QueryVolds.java
@@ -0,0 +1,16 @@
+package com.sample.domin;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class QueryVolds {
+    private List<Integer> ids;
+    public List<Integer> getIds(){
+        return ids;
+    }
+
+    public void setIds(List<Integer> ids) {
+        this.ids = ids;
+    }
+}
